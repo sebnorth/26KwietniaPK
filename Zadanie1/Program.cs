707960a93aed30c3d87b4f8faaf6017e67a83fe0
sebnorth <sebnorth@gmail.com>
2020-04-26 08:37:03 +0200

    Dodaj pliki projektów.

---
+++ Zadanie1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double litry = (132 * 6.5) / 100;
+            double koszt = litry * 3.29;
+            Console.WriteLine(koszt);
+        }
+    }
+}
