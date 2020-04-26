a9264c43d4cc169d442b0d3f1284378874dedc1e
sebnorth <sebnorth@gmail.com>
2020-04-26 08:49:51 +0200

    Dodano drugie zadanie.

---
+++ Zadanie2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int.TryParse(Console.ReadLine(), out int a);
+            // int.TryParse(Console.ReadLine(), out int b);
+            int b = Convert.ToInt32(Console.ReadLine());
+            if (a % b == 0)
+            {
+                Console.WriteLine($"Liczba {a} jest podzielna przez liczbę {b}");
+            }
+            else {
+                Console.WriteLine($"Liczba {a} NIE jest podzielna przez liczbę {b}");
+            }
+
+
+        }
+    }
+}
