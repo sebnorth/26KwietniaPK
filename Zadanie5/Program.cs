061c520cf3b1a17a7c413b293fc84489b2403440
sebnorth <sebnorth@gmail.com>
2020-04-26 09:27:10 +0200

    Dodano kolejne zadania.

---
+++ Zadanie5/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 1;
+            int s = 0;
+
+            while (i<=10)
+            {
+                s += i;
+                i++;
+            }
+
+            Console.WriteLine(s);
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Zadanie6/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int s = 0;
+            for (int i = 1; i <= 10; i++)
+            {
+                s = s + i;
+            }
+
+            Console.WriteLine(s);
+        }
+    }
+}
